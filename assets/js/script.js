7d9ed81de694b4bf76869278d43ad44ded1f7ba7
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-04-15 10:46:38 +0000

    added images and three examples to the wordlist array, created function to display buttons with letters on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
 const gameContainer=document.getElementById("gameContainer")
 const startBtn= document.getElementById("startBtn")
+const letterBtnEL=document.getElementById("letterbtn")
+const wordSpaces= document.getElementById("wordSpaces")
+
+const letters = ["A", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
+function displayBtn(){
+    for(let i=0; i<letters.length; i++){
+        const letterBtn = document.createElement("button")
+        letterBtn.textContent= letters[i]
+        letterBtn.setAttribute("value", letters[i])
+        letterBtnEL.append(letterBtn)
+
+    }
+}
 
 startBtn.addEventListener("click", (event)=>{
     event.preventDefault()
     gameContainer.classList.remove("is-hidden")
+    displayBtn()
 })
---
+++ assets/js/words.js
@@ -0,0 +1,18 @@
+const wordList=[{
+    movie:"PULP FICTION",
+    hint: "I love you honey bunny",
+    imgHint:"../images/pulp fiction.jpg"
+},{
+    movie:"BIG LEBOWSKI",
+    hint:" Dude",
+    imgHint:"../images/The_Dude5.0.jpg"
+},{
+    movie:"FIGHT CLUB",
+    hint:"tbd",
+    imgHint:"../images/fight-club.jpg"
+},
+{
+    movie:"PRETTY WOMAN",
+    hint:"tbd",
+    imgHint:"../images/Pretty-woman.jpg"
+}]
