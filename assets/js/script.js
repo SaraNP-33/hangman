6455e95c49a2782ba389da0e6611b6d6a7f48e77
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-04-20 23:11:55 +0000

    updated css link

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,11 @@ const wordSpaces= document.getElementById("wordSpaces")
 
 const letters = ["A", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+let randomWord;
+let wordStatus=null
+const underscore=[]
+let i=0;
+
 
 function displayBtn(){
     for(let i=0; i<letters.length; i++){
@@ -17,10 +22,27 @@ function displayBtn(){
     }
 }
 
+function word(){
+    
+    if(wordList.length>0){
+        const randomIndex=  Math.floor(Math.random() * wordList.length);
+        console.log(randomIndex)
+        randomWord= wordList[randomIndex].movie.split("")
+        console.log(randomWord)
+    }
+    // still have to figure out how to remove the underscore from the blankSpace
+    wordStatus = randomWord.map(letter => (underscore.indexOf(letter) >= 0 ? letter : ' _ ')).join(' ')
+   
+        console.log(wordStatus)
+
+       wordSpaces.innerHTML = wordStatus
+}
+
 startBtn.addEventListener("click", (event)=>{
     event.preventDefault()
     gameContainer.classList.remove("is-hidden")
     displayBtn()
+    word()
 })
 
 letterBtnEL.addEventListener("click", (event)=>{
@@ -28,3 +50,12 @@ letterBtnEL.addEventListener("click", (event)=>{
     const clickedLetter= this.event.target.value
     console.log(clickedLetter)
 })
+
+
+
+
+
+
+
+// wordStatus = randomWord.map(letter => ((guessed.indexOf(letter) >= 0 && letter!==" ") ? letter : ' _ ')).join(' ');
+    // console.log(wordStatus)
