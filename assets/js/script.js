a3735c398cb90117ccaa0fbd2ac157517a544724
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2023-06-21 16:49:13 +0000

    added blank space to movies with two words

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,12 @@ const underscore=[]
 const guessed=[]
 let i=0;
 
+//TODO: figure out how to include the space between words on the display
+//TODO: create logic for guesses that are wrong 
+//TODO: create logic for the end of the game
+//TODO: add wins/loses to localStorage
+//Todo: create a restart button
+//Todo: include hints
 
 function displayBtn(){
     for(let i=0; i<alphabet.length; i++){
@@ -18,7 +24,8 @@ function displayBtn(){
         letterBtn.setAttribute("value", alphabet[i])
         letterBtn.setAttribute("class"," button alphaBtn mt-2 mx-1")
         letterBtnEL.append(letterBtn)
-
+        
+        
     }
 }
 
@@ -32,6 +39,7 @@ function movie(){
         randomWord = new Word(randomWord);
         console.log(randomWord)
         wordSpaces.innerHTML=randomWord.display()
+        
        
     }
   
@@ -43,6 +51,8 @@ function checkLetter(userChoice){
            guessed.push(userChoice)
           randomWord.check(userChoice)
           wordSpaces.innerHTML=randomWord.display()
+          
+         
            
         } 
         else{
@@ -56,6 +66,7 @@ function checkLetter(userChoice){
 startBtn.addEventListener("click", (event)=>{
     event.preventDefault()
     gameContainer.classList.remove("is-hidden")
+    startBtn.style.display="none"
     displayBtn()
     movie()
 })
@@ -63,7 +74,6 @@ startBtn.addEventListener("click", (event)=>{
 letterBtnEL.addEventListener("click", (event)=>{
     event.preventDefault()
     const clickedLetter= this.event.target.value
-    // console.log(clickedLetter)
     checkLetter(clickedLetter)
 })
 
