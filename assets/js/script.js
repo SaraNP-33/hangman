4d009395cb11d289f92b0ad6829ecbb31eea4a29
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2023-06-21 22:10:52 +0000

    added logic to add used and wrong letters to the right container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 const gameContainer=document.getElementById("gameContainer")
 const startBtn= document.getElementById("startBtn")
 const letterBtnEL=document.getElementById("letterbtn")
+const badLetters=document.getElementById("badLetters")
 let wordSpaces= document.getElementById("wordSpaces")
 
-const alphabet = ["A", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const alphabet = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 let randomWord;
 const underscore=[]
 const guessed=[]
 let i=0;
 
-//TODO: figure out how to include the space between words on the display
-//TODO: create logic for guesses that are wrong 
+
 //TODO: create logic for the end of the game
 //TODO: add wins/loses to localStorage
 //Todo: create a restart button
@@ -23,11 +23,10 @@ function displayBtn(){
         letterBtn.textContent= alphabet[i]
         letterBtn.setAttribute("value", alphabet[i])
         letterBtn.setAttribute("class"," button alphaBtn mt-2 mx-1")
-        letterBtnEL.append(letterBtn)
-        
-        
+        letterBtnEL.append(letterBtn)  
     }
 }
+
 
 function movie(){
     
@@ -46,17 +45,18 @@ function movie(){
 }
 
 function checkLetter(userChoice){
+    randomWord.check(userChoice)
+    wordSpaces.innerHTML=randomWord.display() 
+
         if(!guessed.includes(userChoice)){
             console.log(userChoice);
-           guessed.push(userChoice)
-          randomWord.check(userChoice)
-          wordSpaces.innerHTML=randomWord.display()
-          
-         
-           
+           guessed.push(userChoice) 
         } 
-        else{
-            console.log("try again");
+        if(randomWord.display()){
+            const btn= document.createElement("button")
+            btn.textContent=userChoice;
+            btn.setAttribute("class"," button alphaBtn mt-2 mx-1")
+            badLetters.append(btn)
         }
         
     
@@ -67,7 +67,7 @@ startBtn.addEventListener("click", (event)=>{
     event.preventDefault()
     gameContainer.classList.remove("is-hidden")
     startBtn.style.display="none"
-    displayBtn()
+    // displayBtn()
     movie()
 })
 
@@ -83,5 +83,3 @@ letterBtnEL.addEventListener("click", (event)=>{
 
 
 
-// wordStatus = randomWord.map(letter => ((guessed.indexOf(letter) >= 0 && letter!==" ") ? letter : ' _ ')).join(' ');
-    // console.log(wordStatus)
