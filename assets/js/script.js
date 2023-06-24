3f9c90ffddaded3472d49a0c5ff6fc61053af6f0
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2023-06-24 20:12:17 +0000

    added modal. modal is triggered if user can guess the word

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,46 @@
-const gameContainer=document.getElementById("gameContainer")
-const startBtn= document.getElementById("startBtn")
-const letterBtnEL=document.getElementById("letterbtn")
-const badLetters=document.getElementById("badLetters")
-let wordSpaces= document.getElementById("wordSpaces")
-const guessesLeft= document.getElementById("guessesLeft")
+const gameContainer = document.getElementById("gameContainer");
+const startBtn = document.getElementById("startBtn");
+const letterBtnEL = document.getElementById("letterbtn");
+const badLetters = document.getElementById("badLetters");
+let wordSpaces = document.getElementById("wordSpaces");
+const guessesLeft = document.getElementById("guessesLeft");
+const modal= document.querySelector(".modal")
+const modalTitle= document.querySelector(".winner-loser-title")
+const modalBody= document.querySelector(".winner-loser-body")
 
-const alphabet = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
 let randomWord;
-const underscore=[]
-const guessed=[]
-let i=0;
-let remainaingGuesses=10;
-
+const wrongGuesses = [];
+let remainaingGuesses = 10;
+const jsConfetti = new JSConfetti()
 
 //TODO: Find how to create the alphabet letters in the Letter constructor
 //TODO: create logic for the end of the game
@@ -20,80 +48,82 @@ let remainaingGuesses=10;
 //Todo: create a restart button
 //Todo: include hints
 
-function displayBtn(){
-    for(let i=0; i<alphabet.length; i++){
-        const letterBtn = document.createElement("button")
-        letterBtn.textContent= alphabet[i]
-        letterBtn.setAttribute("value", alphabet[i])
-        letterBtn.setAttribute("class"," button alphaBtn mt-2 mx-1")
-        letterBtnEL.append(letterBtn)  
-    }
-}
-guessesLeft.textContent= `Guesses Left: ${remainaingGuesses}`
+guessesLeft.textContent = `Guesses Left: ${remainaingGuesses}`;
 
-function movie(){
-    
-    if(wordList.length>0){
-        const randomIndex=  Math.floor(Math.random() * wordList.length);
-    
-        randomWord= wordList[randomIndex].movie
-        randomWord = new Word(randomWord);
-
-       
-        
-        wordSpaces.innerHTML=randomWord.display()
-        
-       
-    }
-  
+function displayBtn() {
+  for (let i = 0; i < alphabet.length; i++) {
+    const letterBtn = document.createElement("button");
+    letterBtn.textContent = alphabet[i];
+    letterBtn.setAttribute("value", alphabet[i]);
+    letterBtn.setAttribute("class", " button alphaBtn mt-2 mx-1");
+    letterBtnEL.append(letterBtn);
+  }
 }
 
-function checkLetter(userChoice){
-    randomWord.check(userChoice)
-    wordSpaces.innerHTML=randomWord.display() 
-     const show= randomWord.display()
-     console.log(show);    
-    
 
-        // if(!guessed.includes(userChoice)){
-        //     console.log(userChoice);
-        //    guessed.push(userChoice) 
-        // } 
-        
-        if(!show.includes(userChoice) && !guessed.includes(userChoice) ){
-            console.log("try again");
-            guessed.push(userChoice) 
-            const btn= document.createElement("button")
+function movie() {
+  if (wordList.length > 0) {
+    const randomIndex = Math.floor(Math.random() * wordList.length);
 
-            guessed.forEach(index => {
-                btn.textContent=userChoice;
-                btn.setAttribute("class"," button alphaBtn mt-2 mx-1")
-                badLetters.append(btn)
-            });
-        
-            remainaingGuesses--
-             guessesLeft.textContent= `Guesses Left: ${remainaingGuesses}`
-            console.log(remainaingGuesses);
-        }      
+    randomWord = wordList[randomIndex].movie;
+    randomWord = new Word(randomWord);
+
+    wordSpaces.innerHTML = randomWord.display();
+  }
 }
 
-startBtn.addEventListener("click", (event)=>{
-    event.preventDefault()
-    gameContainer.classList.remove("is-hidden")
-    startBtn.style.display="none"
-    displayBtn()
-    movie()
-})
+function checkLetter(userChoice) {
+  randomWord.check(userChoice);
+  wordSpaces.innerHTML = randomWord.display();
+  const show = randomWord.display();
+  console.log(show);
 
-letterBtnEL.addEventListener("click", (event)=>{
-    event.preventDefault()
-    const clickedLetter= this.event.target.value
-    checkLetter(clickedLetter)
-})
+  if (!show.includes(userChoice) && !wrongGuesses.includes(userChoice)) {
+    console.log("try again");
+    wrongGuesses.push(userChoice);
+    const btn = document.createElement("button");
 
+    wrongGuesses.forEach(() => {
+      btn.textContent = userChoice;
+      btn.setAttribute("class", " button alphaBtn mt-2 mx-1");
+      badLetters.append(btn);
+    });
 
+    remainaingGuesses--;
+    guessesLeft.textContent = `Guesses Left: ${remainaingGuesses}`;
+    console.log(remainaingGuesses);
+  }
 
+  if(!show.includes("_")){
+    modalTitle.textContent="WINNER!!!"
+    modalBody.textContent="You Did it!!!!"
+    jsConfetti.addConfetti({
+      confettiColors: [
+        '#f75ca5', '#34e0e8', '#f4b824', '#f66f35', '#3d40b3', '#74bd78',
+      ],
+    })
+    openModal();
+  }
+}
 
+startBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  gameContainer.classList.remove("is-hidden");
+  startBtn.style.display = "none";
+  displayBtn();
+  movie();
+});
 
+letterBtnEL.addEventListener("click", (event) => {
+  event.preventDefault();
+  const clickedLetter = event.target.value;
+  checkLetter(clickedLetter);
+});
 
+function openModal(){
+  modal.classList.add("is-active")
+}
 
+function closeModal(){
+  modal.classList.remove("is-active")
+}
